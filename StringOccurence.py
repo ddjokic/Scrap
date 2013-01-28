c5b1c944f8c5962e0547bb391490cea4031baf5d
ddjokic <deki.djokic@gmail.com>
2013-01-28 18:58:34 +0800

    Just started

---
+++ StringOccurence.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#counts occurrence of the strings in *.txt file - extension required
+
+fname=raw_input("Enter filename with extension where data resides: ")
+nos=int(input("Enter numbers of strings to be counted: "))
+f=open(fname, 'r')
+str=f.read()
+for broj in range (1, nos+1):
+	print ("String %r") %broj
+	string=raw_input("Enter string: ")
+	cnt=str.count(string)
+	print ("%r : %r") %(string, cnt)
+print ("Done!")
---
+++ readme.md
@@ -0,0 +1,4 @@
+<h4>Scrap - collection of small useful scripts</h4>
+Useful for me.</p>
+No any warranties!!!</p>
+StringOccurence.py - counts strings occurences in *.txt file
