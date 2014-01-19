e16973770fa6e4fa8b873f6bdfa02024bd8589ef
ddjokic <deki.djokic@gmail.com>
2014-01-19 14:41:26 +0800

    Updated filelist.py

--- filelist.py
+++ filelist.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
-
-import os
-dir = raw_input('Enter full directory path: ')
-os.chdir(dir)
-allfiles = os.listdir(dir)
-#print (allfiles)
-
-filename="000-files_list.txt"
-fn=open(filename, 'a')
-fn.write(dir)
-for fname in allfiles:
-	fn.write("\n")
-	fn.write(fname)
-fn.close()
+'''list files in filder and all sub-folders and writing result
+	in 000-file_list.txt, located in folder examined.
+	feel free to modify, as you see fits
+	(c) 2014, D. Djokic. No warranties'''
 	
+import os
+
+path = raw_input ("Enter full directory path: ")
+os.chdir(path)
+filename = "000-file_list.txt"
+fn = open(filename, "w")
+
+for root, dirs, files in os.walk (path, topdown=True):
+	fn.write (str(root))
+	fn.write ("\n")
+	fn.write (str(dirs))
+	
+	for file in files:
+		
+		fn.write("\n")
+		fn.write(str(file))
+
+fn.close()
